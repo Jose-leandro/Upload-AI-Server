2b3de0d2d07db0fdd2bfdd9079a41a4bba868a14
Joseleandro7i <lenadrojose124leandro@gmail.com>
2023-09-19 09:06:03 -0300

    Atualização no package.json e exclusão do arquivo vercel.json, adição de routes.http e atualização do server.ts

--- src/server.ts
+++ src/server.ts
@@ -17,10 +17,10 @@ app.register(uploadVideoRouter)
 app.register(createTranscriptionRoute)
 app.register(generateAICompletionRoute)
 
-// const port = process.env.PORT || 3000 ;
 
 app.listen({
-    port: 3333,
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }).then(() => {
     console.log("HTTP Server Running in port: http://localhost:3333/");
 })
